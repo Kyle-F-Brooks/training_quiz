0e2b4f5f2fdd6f1c909599052615ed2eddad3864
Kyle Brooks <Kyle-F-Brooks@users.noreply.github.com>
2020-11-16 18:34:39 +0000

    added Two new components and added bootstrap

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-jumbotron header="Questions" lead="">
+            <p>filler</p>
+            <b-button variant="primary" href="#">More Info</b-button>
+        </b-jumbotron>
+    </div>
+</template>
